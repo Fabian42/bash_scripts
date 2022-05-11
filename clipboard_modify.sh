579ec1d617feb6ae4295859338b382099c9f9a80
Fabian42 <fabianroeling@googlemail.com>
2022-05-11 14:10:07 +0200

    My previous genius change broke backslashes, this fixed that.

--- clipboard_modify.sh
+++ clipboard_modify.sh
@@ -4,12 +4,12 @@ while clipnotify; do
  new="$(xsel -bo)"
  # remove trash from copied YouTube links
  if [[ "$new" == "https://www.youtube.com/watch?v="* ]]; then
-  echo -n "$new" | sed "s/\\&list\\=[a-zA-Z0-9\\_\\-]+\\&index\\=[0-9]+//g" | xsel -bi
+  echo -En "$new" | sed "s/\\&list\\=[a-zA-Z0-9\\_\\-]+\\&index\\=[0-9]+//g" | xsel -bi
  # remove trash from copies Twitch clip links
  elif [[ "$new" == "https://www.twitch.tv/"*"/clip/"* ]]; then
-  echo -n "$new" | sed "s/\\?filter\\=clips\\&range\\=all\\&sort\\=time//g" | xsel -bi
+  echo -En "$new" | sed "s/\\?filter\\=clips\\&range\\=all\\&sort\\=time//g" | xsel -bi
  elif xsel -bo | file - | grep text >/dev/null; then
   # ensure that clipboard doesn't get cleared when closing a program
-  echo -n "$new" | xsel -bi
+  echo -En "$new" | xsel -bi
  fi
 done
